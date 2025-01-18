b223c13a4917bb57a3a4d25c50104ee95d6d2e96
unknown <santimallalakshmidhar@gmail.com>
2025-01-18 09:42:47 +0530

    add all components

---
+++ src/components/Aboutus.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Aboutus(){
+    return(
+    <>
+    ABOUTUs
+    </>
+    )
+}
+export default Aboutus;
---
+++ src/components/Activities.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Activities(){
+    return(
+<>
+Activites
+</>
+    )
+}
+
+export default Activities;
