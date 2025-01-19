c2ff577d7c72fe3babdfa14ef3f8831d3eb4d87d
Sachin-2006 <sachin06kaizen@gmail.com>
2025-01-19 16:23:14 +0530

    worked on navbar and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import "../css/Footer.css"
+
+import { useRef } from "react";
+
+
+function Footer(){
+    
+
+
+    return(<>
+
+            <hr />
+    <footer>
+    <div className="logo"> <img className='logo-img' src="/src/assets/Logo.png" alt="" />82</div>
+    Fooooooooooooter!!!!
+    </footer>
+    </>)
+}
+
+export default Footer;
