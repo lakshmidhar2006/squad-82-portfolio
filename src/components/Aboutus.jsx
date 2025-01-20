8b994348733648ba421314a98a66e1aec1404e4d
unknown <santimallalakshmidhar@gmail.com>
2025-01-20 08:37:03 +0530

    sdg

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "../css/AboutUs.css";
-import grouppic from "../assets/group picture.jpg"; 
+import grouppic from "../assets/group picture.jpg";
+import grouppic2 from "../assets/picture 2.jpg";
+import grouppic3 from "../assets/picture 3.jpg";
 
 const AboutUs = () => {
   return (
     <div className="about-us">
-      <img
-        src={grouppic} 
-        alt="Group Picture"
-        className="large-image"
-      />
+      <div className="big-image-container">
+        <img src={grouppic} alt="Group Picture 1" className="image big-image" />
+      </div>
+      <div className="small-images-container">
+        <img src={grouppic2} alt="Group Picture 2" className="image small-image" />
+        <img src={grouppic3} alt="Group Picture 3" className="image small-image" />
+      </div>
     </div>
   );
 };
